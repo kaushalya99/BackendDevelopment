42d590acd81f21499948c131a5d9e4d2d98b01f1
kaushalya99 <hasheweerasooriya@gmail.com>
2024-09-11 22:42:16 +0530

    MovieReview created.

--- movies/src/main/java/dev/farhan/movies/MovieRepository.java
+++ movies/src/main/java/dev/farhan/movies/MovieRepository.java
@@ -1,4 +1,10 @@
 package dev.farhan.movies;
 
-public class MovieRepository {
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId>{
+
 }
